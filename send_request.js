3ebcd3e2cc2f73c6605b886101001b2726973a12
rickharris-dev <rickharris724@gmail.com>
2016-06-27 13:38:45 -0700

    Removed console logging for tests

--- send_request.js
+++ send_request.js
@@ -1,12 +1,11 @@
 $(function() {
     $('#submit').click(function() {
-        console.log("This is being called")
-        console.log($('form').serialize())
-        var url = 'http://cp-0627-2.hbtn.io:8091/wiki/'
-        var data = $('form').serialize()
-        $.post(url, data, function(response) {
+        $.ajax({
+            url: 'http://cp-0627-2.hbtn.io:8091/wiki/',
+            data: $('form').serialize(),
+            type: 'POST',
+            success: function(response) {
                 // $('#progress').css("display","block");
-				console.log("hi")
                 // In case of success, we should display the result
                 // For now we can debug with console response <-- return value of go function
                 console.log(response);
@@ -14,6 +13,10 @@ $(function() {
                 /*
                 $("textarea").html(// code to display result here)
                 */
+            },
+            error: function(error) {
+                console.log(error);
+            }
         });
     });
 });
