c74f0c501f36fafaf0b65de4c349d0a96050bccd
butcallmejo <josquin.gaillard@gmail.com>
2016-06-27 11:48:53 -0700

    hopefully sen request js works now

--- send_request.js
+++ send_request.js
@@ -3,15 +3,15 @@ $(function() {
 
         $.ajax({
             url: 'http://cp-0627-2.hbtn.io:8091/wiki',
-            data: $('form').serialize(),
+            data: $('#form').serialize(),
             type: 'POST',
             success: function(response) {
                 // $('#progress').css("display","block");
-                
+
                 // In case of success, we should display the result
                 // For now we can debug with console response <-- return value of go function
                 console.log(response);
-                
+
                 /*
                 $("textarea").html(// code to display result here)
                 */
