f531f64b6c3efc8ab72476d521ee5bbfb6e23020
Chandler Mayo <chandler@chandlermayo.com>
2016-06-27 14:26:10 -0700

    Added pathfinder and recompiled

--- API/main.go
+++ API/main.go
@@ -3,14 +3,10 @@ package main
 import (
     "os"
     "fmt"
-    "time"
-    "sync"
-    "regexp"
     "net/http"
-    "io/ioutil"
-    "strings"
     "github.com/gorilla/mux"
     "github.com/gorilla/handlers"
+    "os/exec"
 )
 
 func main() {
@@ -29,88 +25,15 @@ var FindHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
     r.ParseForm()
     Start_l := r.FormValue("first")
     End_l := r.FormValue("second")
-
     if ((len(Start_l) > 0) && (len(End_l) > 0)) {
-
         fmt.Println("Path find: " + Start_l + " " + End_l)
-
-        reFindLink, err := regexp.Compile("href=\"(/wiki/[^\"/ :#]*)\"")
-
-        if err != nil {
-            fmt.Println(err)
-            return
-        }
-
-        mutex := new(sync.Mutex)
-        visited := make(map[string]bool)
-        base_link := "https://en.wikipedia.org"
-        http_start := []string{"/wiki/" + Start_l}
-        link_goal := "/wiki/" + End_l
-        done := false
-        level := 1
-        u := 1
-        thread_count := 100
-        var solution []string
-
-        queue := make(chan []string, 100000000)
-
-        queue <- http_start
-
-        for i := 0; i < thread_count; i++ {
-            go func (id int) {
-                defer func() {
-                    thread_count--
-                } ()
-                for L := range queue {
-                    if len(L) > level {
-                        level++
-                    }
-                    req, err := http.Get(base_link + L[len(L)-1])
-                    if err == nil {
-                        b, _ := ioutil.ReadAll(req.Body)
-                        links := reFindLink.FindAllStringSubmatch(string(b), -1)
-                        mutex.Lock()
-                        if !done {
-                            for t := range links {
-                                _, v := visited[links[t][1]]
-                                if links[t][1] == link_goal {
-                                    solution = append(L, links[t][1])
-                                    done = true
-                                    mutex.Unlock()
-                                    return
-                                }
-                                if !v {
-                                    visited[links[t][1]] = true
-                                    queue <- append(L, links[t][1])
-                                }
-                            }
-                            if (u < 1000) {
-                                u = u+1
-                            } else {
-                                mutex.Unlock()
-                                done = true
-                                return
-                            }
-                        }
-                        mutex.Unlock()
-                    } 
-                    if done {
-                        return
-                    }
-                    time.Sleep(10);
-                }
-                return;
-            }(i)
-        }
-        for !done || thread_count > 0 {
-            time.Sleep(1)
-        }
-        if ( u < 1000) {
-            stringfinal := strings.Join(solution, ", ")
-            w.Write([]byte(stringfinal))
+        out, err := exec.Command("/api/pathfinder", Start_l, End_l).Output()
+        if err == nil {
+            fmt.Println(out)
+            w.Write([]byte(out))
         } else {
-            fmt.Println("Error - bad data or could not find a path.")
-            w.Write([]byte("Error - bad data or could not find a path."))
+            fmt.Println(err)
+            w.Write([]byte("Error - pathfinder did not run."))
         }
     } else {
         fmt.Println("Error - missing data.")
