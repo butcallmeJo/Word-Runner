3cf86bef973e0851b32d593888b228c2ecbd1ff2
butcallmejo <josquin.gaillard@gmail.com>
2016-06-27 14:21:16 -0700

    show/hide progress bar jQuery

--- send_request.js
+++ send_request.js
@@ -1,5 +1,6 @@
 $(function() {
     $('#submit').click(function() {
+		$('#progress').show()
         $.ajax({
             url: 'http://cp-0627-2.hbtn.io:8091/wiki/',
             data: $('form').serialize(),
@@ -9,15 +10,14 @@ $(function() {
                 // In case of success, we should display the result
                 // For now we can debug with console response <-- return value of go function
                 console.log(response);
+				$('#progress').hide()
                 $("#result").html(response)
-
 
                 // var str1 = "<p>"
                 // var str2 = str1.concat(response)
                 // var printable_result = str2.concat(str1)
                 //
                 // $("result").html(printable_result)
-
 
             },
             error: function(error) {
